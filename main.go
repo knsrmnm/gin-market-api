75186ca9699f637d2e5b99a959d9ba48c5b42a8c
Ken Minami <kenminami.dev@gmail.com>
2025-01-31 22:44:43 +0900

    create item model

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-market-api/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run("localhost:8080")
+	items := []models.Item{
+		{ID: 1, Name: "product1", Price: 1000, Description: "sample data", SoldOut: false},
+		{ID: 2, Name: "product2", Price: 2000, Description: "sample data", SoldOut: true},
+		{ID: 3, Name: "product3", Price: 3000, Description: "sample data", SoldOut: false},
+	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("localhost:8080")
 }
