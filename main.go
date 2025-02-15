f2172e5138256014eb08bdc7978c138be493cf70
Ken Minami <kenminami.dev@gmail.com>
2025-02-15 10:50:19 +0900

    ユーザー作成,サインアップ

--- main.go
+++ main.go
@@ -17,11 +17,21 @@ func main() {
 	itemService := services.NewItemService(itemRepository)
 	ItemController := controllers.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	r := gin.Default()
-	r.GET("/items", ItemController.FindAll)
-	r.GET("/items/:id", ItemController.FindById)
-	r.POST("/items", ItemController.Create)
-	r.PUT("/items/:id", ItemController.Update)
-	r.DELETE("/items/:id", ItemController.Delete)
+	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+
+	itemRouter.GET("", ItemController.FindAll)
+	itemRouter.GET("/:id", ItemController.FindById)
+	itemRouter.POST("", ItemController.Create)
+	itemRouter.PUT("/:id", ItemController.Update)
+	itemRouter.DELETE("/:id", ItemController.Delete)
+
+	authRouter.POST("/signup", authController.Signup)
+
 	r.Run("localhost:8080")
 }
