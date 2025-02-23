95d24ac68c9005ad73d8ee78d1bf997bd88ece06
Ken Minami <kenminami.dev@gmail.com>
2025-02-23 16:27:51 +0900

    cors追加

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"gin-market-api/repositories"
 	"gin-market-api/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,6 +24,7 @@ func main() {
 	authController := controllers.NewAuthController(authService)
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	itemRouter := r.Group("/items")
 	itemRouterWithAuth := r.Group("/items", middlewares.AuthMiddleware(authService))
 	authRouter := r.Group("/auth")
